66cebfcccb1fe9f93659ccba668952e38f04cbb8
HoJeongKimmm <2020111556@dgu.ac.kr>
2024-08-02 16:15:42 +0900

    Add : 의존성 추가

--- README.md
+++
@@ -1,2 +0,0 @@
-# springboot_codingapple
-코딩 애플 스프링 부트 강의
---
+++ src/test/java/com/hojeong/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hojeong.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
